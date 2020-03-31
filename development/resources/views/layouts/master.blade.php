55faf790ca521a2531c0b1c960ac2fc85f0d61bf
Tharanga Weerappullige <tharangasadamali94@gmail.com>
2020-03-31 22:36:40 +0530

    set up basics

---
+++ development/resources/views/layouts/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>@yield('title')</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	@yield('styles')
+</head>
+<body>
+	@yield('content')
+
+	<script
+  src="https://code.jquery.com/jquery-1.12.4.js"
+  integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="
+  crossorigin="anonymous"></script>
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	@yield('scripts')
+</body>
+</html>
---
+++ development/resources/views/shop/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('title')
+Laravel eShop
+@endsection
+
+@section('content')
+<h1>It works</h1>
+
+@endsection
