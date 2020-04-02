316f02e5200ddb2b85cb28da99df06acc72b152e
Tharanga Weerappullige <tharangasadamali94@gmail.com>
2020-04-02 08:04:52 +0530

    create navbar

--- development/resources/views/layouts/master.blade.php
+++ development/resources/views/layouts/master.blade.php
@@ -3,18 +3,25 @@
 <head>
 	<meta charset="utf-8">
 	<title>@yield('title')</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
 	@yield('styles')
 </head>
 <body>
-	@yield('content')
+	@include('partials.header')
+	<div class="container">
+		@yield('content')
+	</div>
+	
 
-	<script
+<script
   src="https://code.jquery.com/jquery-1.12.4.js"
   integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="
   crossorigin="anonymous"></script>
 
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	@yield('scripts')
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+@yield('scripts')
 </body>
 </html>
--- development/resources/views/shop/index.blade.php
+++ development/resources/views/shop/index.blade.php
@@ -5,6 +5,20 @@ Laravel eShop
 @endsection
 
 @section('content')
-<h1>It works</h1>
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="http://www.fondation-louisbonduelle.org/wp-content/uploads/2016/10/carotte_222805396.png" alt="...">
+      <div class="caption">
+        <h3>Product Title</h3>
+        <p>ABCDEF</p>
+        <div>
+        	<a href="#" class="btn btn-default" role="button">Button</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 @endsection
