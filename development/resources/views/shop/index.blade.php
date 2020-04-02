33e4afff4bfe7547891f4e28d17ad8faa7bab1ca
Tharanga Weerappullige <tharangasadamali94@gmail.com>
2020-04-02 09:13:11 +0530

    shopping page create

--- development/resources/views/shop/index.blade.php
+++ development/resources/views/shop/index.blade.php
@@ -8,12 +8,51 @@ Laravel eShop
 <div class="row">
   <div class="col-sm-6 col-md-4">
     <div class="thumbnail">
-      <img src="http://www.fondation-louisbonduelle.org/wp-content/uploads/2016/10/carotte_222805396.png" alt="...">
+      <img src="https://static-s3.supermart.ng/productImage/1456847659916_omwpxyz12_carrot_1_kg.jpg" alt="...">
       <div class="caption">
-        <h3>Product Title</h3>
-        <p>ABCDEF</p>
-        <div>
-        	<a href="#" class="btn btn-default" role="button">Button</a>
+        <h3>Carrot</h3>
+        <div class="pull-left price">Rs. 100</div>
+        <div class="clearfix">
+        	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="https://static-s3.supermart.ng/productImage/1456847659916_omwpxyz12_carrot_1_kg.jpg" alt="...">
+      <div class="caption">
+        <h3>Carrot</h3>
+        <div class="pull-left price">Rs. 100</div>
+        <div class="clearfix">
+        	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="https://static-s3.supermart.ng/productImage/1456847659916_omwpxyz12_carrot_1_kg.jpg" alt="...">
+      <div class="caption">
+        <h3>Carrot</h3>
+        <div class="pull-left price">Rs. 100</div>
+        <div class="clearfix">
+        	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="https://static-s3.supermart.ng/productImage/1456847659916_omwpxyz12_carrot_1_kg.jpg" alt="...">
+      <div class="caption">
+        <h3>Carrot</h3>
+        <div class="pull-left price">Rs. 100</div>
+        <div class="clearfix">
+        	<a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
         </div>
       </div>
     </div>
